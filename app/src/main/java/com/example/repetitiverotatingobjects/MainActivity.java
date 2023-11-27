53cd771a6cbf90c52419e270df182eba38dfea17
Chirs <christopherma597@icloud.com>
2023-11-27 10:59:20 -0600

    Added start screen and button and back arrow

--- app/src/main/java/com/example/repetitiverotatingobjects/MainActivity.java
+++ app/src/main/java/com/example/repetitiverotatingobjects/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.repetitiverotatingobjects;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Point;
@@ -13,95 +14,17 @@ import android.view.WindowManager;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends Activity {
-    TetrisCtrl mTetrisCtrl;
-    Point mScreenSize = new Point(0, 0);
-    Point mMousePos = new Point(-1, -1);
-    int mCellSize = 0;
-    boolean mIsTouchMove = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-
-        DisplayMetrics dm = this.getApplicationContext().getResources().getDisplayMetrics();
-        mScreenSize.x = dm.widthPixels;
-        mScreenSize.y = dm.heightPixels;
-        mCellSize = (int)(mScreenSize.x / 8);
-
-        initTetrisCtrl();
     }
-
-    void initTetrisCtrl() {
-        mTetrisCtrl = new TetrisCtrl(this);
-        for(int i=0; i <= 7; i++) {
-            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.cell0 + i);
-            mTetrisCtrl.addCellImage(i, bitmap);
-        }
-        RelativeLayout layoutCanvas = findViewById(R.id.layoutCanvas);
-        layoutCanvas.addView(mTetrisCtrl);
-    }
-
-    @SuppressLint("NonConstantResourceId")
-    public void onClick(View v) {
-        int viewId = v.getId();
-        if(viewId == R.id.btnLeft){
-            mTetrisCtrl.block2Left();
-        }
-        else if(viewId == R.id.btnRight){
-            mTetrisCtrl.block2Right();
-        }
-        else if(viewId == R.id.btnBottom){
-            mTetrisCtrl.block2Bottom();
-        }
-    }
-
-    public boolean onTouchEvent(MotionEvent event) {
-        super.onTouchEvent(event);
-        switch( event.getAction() ) {
-            case MotionEvent.ACTION_DOWN :
-                mIsTouchMove = false;
-                if( event.getY() < (int)(mScreenSize.y * 0.75)) {
-                    mMousePos.x = (int) event.getX();
-                    mMousePos.y = (int) event.getY();
-                }
-                break;
-            case MotionEvent.ACTION_MOVE :
-                if( mMousePos.x < 0 )
-                    break;
-                if( (event.getX() - mMousePos.x) > mCellSize ) {
-                    mTetrisCtrl.block2Right();
-                    mMousePos.x = (int) event.getX();
-                    mMousePos.y = (int) event.getY();
-                    mIsTouchMove = true;
-                } else if( (mMousePos.x - event.getX()) > mCellSize ) {
-                    mTetrisCtrl.block2Left();
-                    mMousePos.x = (int) event.getX();
-                    mMousePos.y = (int) event.getY();
-                    mIsTouchMove = true;
-                }
-                break;
-            case MotionEvent.ACTION_UP :
-                if( mIsTouchMove == false && mMousePos.x > 0 )
-                    mTetrisCtrl.block2Rotate();
-                mMousePos.set(-1, -1);
-                break;
-        }
-        return true;
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        mTetrisCtrl.pauseGame();
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        mTetrisCtrl.restartGame();
+    public void switchScreens(View v){
+        Intent intentBack = new Intent(MainActivity.this, TetrisPlay.class);
+        startActivity(intentBack);
+        finish();
     }
 }
